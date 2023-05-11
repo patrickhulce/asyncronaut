971a18082f9c7b8642705932a4c009431bb06cb0
Patrick Hulce <patrick.hulce@gmail.com>
2023-05-10 22:49:34 -0500

    feat: add promise utilities

---
+++ src/common/promises.test.ts
@@ -0,0 +1,399 @@
+import {
+  TimeoutError,
+  AbortError,
+  TimeoutSourceLateRejectionError,
+  createDecomposedPromise,
+  withTimeout,
+  withRetry,
+  flushAllMicrotasks,
+  withInspection,
+} from './promises';
+
+describe(createDecomposedPromise, () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('creates a decomposed promise with exposed resolve and reject methods', () => {
+    const {resolve, reject, promise} = createDecomposedPromise();
+
+    expect(typeof resolve).toBe('function');
+    expect(typeof reject).toBe('function');
+    expect(promise).toBeInstanceOf(Promise);
+  });
+
+  it('creates a decomposed promise with exposed resolve and reject methods', () => {
+    const {resolve, reject, promise} = createDecomposedPromise();
+
+    expect(typeof resolve).toBe('function');
+    expect(typeof reject).toBe('function');
+    expect(promise).toBeInstanceOf(Promise);
+  });
+
+  it('resolves the promise when the resolve method is called', async () => {
+    const {resolve, promise} = createDecomposedPromise();
+
+    setTimeout(() => resolve(42), 100);
+    jest.advanceTimersByTime(101);
+    await expect(promise).resolves.toBe(42);
+  });
+
+  it('rejects the promise when the reject method is called', async () => {
+    const {reject, promise} = createDecomposedPromise();
+
+    setTimeout(() => reject(new Error('Oops!')), 100);
+    jest.advanceTimersByTime(101);
+    await expect(promise).rejects.toThrow('Oops!');
+  });
+
+  it('can resolve the promise with any value', async () => {
+    const {resolve, promise} = createDecomposedPromise();
+
+    const testData = {key: 'value'};
+    setTimeout(() => resolve(testData), 100);
+    jest.advanceTimersByTime(101);
+    await expect(promise).resolves.toBe(testData);
+  });
+
+  it('can reject the promise with any error', async () => {
+    const {reject, promise} = createDecomposedPromise();
+
+    const customError = new TypeError('Custom error');
+    setTimeout(() => reject(customError), 100);
+    jest.advanceTimersByTime(101);
+    await expect(promise).rejects.toThrow(customError);
+  });
+});
+
+describe(withTimeout, () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('returns the original promise if timeoutMs is not finite', async () => {
+    const sourcePromise = Promise.resolve(42);
+    const result = withTimeout(sourcePromise, {timeoutMs: Infinity});
+
+    expect(result).toBe(sourcePromise);
+    await expect(result).resolves.toBe(42);
+  });
+
+  it('rejects with a TimeoutError if the operation exceeds the timeout', async () => {
+    const sourcePromise = new Promise((resolve) => setTimeout(resolve, 200));
+    const result = withTimeout(sourcePromise, {timeoutMs: 100});
+
+    jest.advanceTimersByTime(100);
+    await expect(result).rejects.toBeInstanceOf(TimeoutError);
+  });
+
+  it('rejects with a custom timeout error message if provided', async () => {
+    const sourcePromise = new Promise((resolve) => setTimeout(resolve, 200));
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 100,
+      timeoutErrorMessage: 'Custom timeout message',
+    });
+
+    jest.advanceTimersByTime(100);
+    await expect(result).rejects.toThrow('Custom timeout message');
+  });
+
+  it('resolves if the operation finishes before the timeout', async () => {
+    const sourcePromise = new Promise((resolve) => setTimeout(resolve, 50, 42));
+    const result = withTimeout(sourcePromise, {timeoutMs: 100});
+
+    jest.advanceTimersByTime(50);
+    await expect(result).resolves.toBe(42);
+  });
+
+  it('rejects with an AbortError if the abortController signals abort', async () => {
+    const sourcePromise = new Promise((resolve) => setTimeout(resolve, 200));
+    const abortController = new AbortController();
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 1000,
+      abortController,
+    });
+
+    jest.advanceTimersByTime(50);
+    abortController.abort();
+    await expect(result).rejects.toBeInstanceOf(AbortError);
+  });
+
+  it('rejects with a custom abort error message if provided', async () => {
+    const sourcePromise = new Promise((resolve) => setTimeout(resolve, 200));
+    const abortController = new AbortController();
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 1000,
+      abortController,
+      abortErrorMessage: 'Custom abort message',
+    });
+
+    jest.advanceTimersByTime(50);
+    abortController.abort();
+    await expect(result).rejects.toThrow('Custom abort message');
+  });
+
+  it('invokes abort on the provided abortController when the operation times out', async () => {
+    const sourcePromise = new Promise((resolve) => setTimeout(resolve, 200));
+    const abortController = new AbortController();
+    const spy = jest.spyOn(abortController, 'abort');
+
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 100,
+      abortController,
+    });
+
+    jest.advanceTimersByTime(101);
+    await expect(result).rejects.toBeInstanceOf(TimeoutError);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('calls cleanupOnResolve when the promise resolves after timeout', async () => {
+    const sourcePromise = new Promise((resolve) => setTimeout(resolve, 200, 42));
+    const cleanupOnResolve = jest.fn();
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 100,
+      cleanupOnResolve,
+    });
+
+    jest.advanceTimersByTime(201);
+    await expect(result).rejects.toBeInstanceOf(TimeoutError);
+    expect(cleanupOnResolve).toHaveBeenCalledWith(42);
+  });
+
+  it('calls cleanupOnReject when the promise rejects after timeout', async () => {
+    const error = new Error('Oops!');
+    const sourcePromise = new Promise((_, reject) => setTimeout(reject, 200, error));
+    const cleanupOnReject = jest.fn();
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 100,
+      cleanupOnReject,
+    });
+
+    jest.advanceTimersByTime(201);
+    await expect(result).rejects.toBeInstanceOf(TimeoutError);
+    expect(cleanupOnReject).toHaveBeenCalledWith(error);
+  });
+
+  it('cleanupOnReject handles non-Error reasons', async () => {
+    const nonErrorReason = 'error message';
+    const sourcePromise = new Promise((_, reject) => setTimeout(reject, 200, nonErrorReason));
+    const cleanupOnReject = jest.fn();
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 100,
+      cleanupOnReject,
+    });
+
+    jest.advanceTimersByTime(201);
+    await expect(result).rejects.toBeInstanceOf(TimeoutError);
+
+    expect(cleanupOnReject).toHaveBeenCalledTimes(1);
+    const [error] = cleanupOnReject.mock.calls[0];
+    expect(error).toBeInstanceOf(TimeoutSourceLateRejectionError);
+    if (!(error instanceof TimeoutSourceLateRejectionError)) throw new Error('Impossible');
+    expect(error.originalRejection).toEqual(nonErrorReason);
+  });
+
+  it('does not call cleanupOnResolve when the promise resolves before timeout', async () => {
+    const sourcePromise = new Promise((resolve) => setTimeout(resolve, 50, 42));
+    const cleanupOnResolve = jest.fn();
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 100,
+      cleanupOnResolve,
+    });
+
+    jest.advanceTimersByTime(51);
+    await expect(result).resolves.toBe(42);
+    jest.advanceTimersByTime(50);
+    await flushAllMicrotasks();
+    expect(cleanupOnResolve).not.toHaveBeenCalled();
+  });
+
+  it('does not call cleanupOnReject when the promise rejects before timeout', async () => {
+    const sourcePromise = new Promise((_, reject) => setTimeout(reject, 50, new Error('Oops!')));
+    const cleanupOnReject = jest.fn();
+    const result = withTimeout(sourcePromise, {
+      timeoutMs: 100,
+      cleanupOnReject,
+    });
+
+    jest.advanceTimersByTime(51);
+    await expect(result).rejects.toThrow('Oops!');
+    jest.advanceTimersByTime(50);
+    await flushAllMicrotasks();
+    expect(cleanupOnReject).not.toHaveBeenCalled();
+  });
+});
+
+describe(withRetry, () => {
+  it('returns the result if the action succeeds', async () => {
+    const actionToRetry = jest.fn(() => Promise.resolve(42));
+    const result = await withRetry(actionToRetry, {retries: 2});
+
+    expect(actionToRetry).toHaveBeenCalledTimes(1);
+    expect(result).toBe(42);
+  });
+
+  it('retries the action the specified number of times on failure', async () => {
+    const actionToRetry = jest.fn(() => Promise.reject(new Error('Oops!')));
+    await expect(withRetry(actionToRetry, {retries: 2})).rejects.toThrow('Oops!');
+    expect(actionToRetry).toHaveBeenCalledTimes(3);
+  });
+
+  it('returns the result if the action succeeds after retrying', async () => {
+    const actionToRetryFn = jest
+      .fn()
+      .mockRejectedValueOnce(new Error('Oops!'))
+      .mockRejectedValueOnce(new Error('Oops!'))
+      .mockResolvedValueOnce(42);
+
+    const result = await withRetry(actionToRetryFn, {retries: 2});
+    expect(actionToRetryFn).toHaveBeenCalledTimes(3);
+    expect(result).toBe(42);
+  });
+
+  it('performs cleanup after each failure if provided', async () => {
+    const actionToRetryFn = jest
+      .fn()
+      .mockRejectedValueOnce(new Error('Oops!'))
+      .mockRejectedValueOnce(new Error('Oops!'))
+      .mockResolvedValueOnce(42);
+
+    const cleanup = jest.fn(() => Promise.resolve());
+    const result = await withRetry(actionToRetryFn, {retries: 2, cleanup});
+
+    expect(actionToRetryFn).toHaveBeenCalledTimes(3);
+    expect(cleanup).toHaveBeenCalledTimes(2);
+    expect(result).toBe(42);
+  });
+
+  it('awaits the cleanup before retrying the action', async () => {
+    const actionToRetryFn = jest.fn().mockRejectedValue(new Error('Oops!'));
+    const cleanupDeferrable = createDecomposedPromise<void>();
+    const cleanup = jest.fn(() => cleanupDeferrable.promise);
+
+    const retriedResult = withInspection(withRetry(actionToRetryFn, {retries: 1, cleanup}));
+
+    await flushAllMicrotasks();
+    expect(actionToRetryFn).toHaveBeenCalledTimes(1);
+    expect(retriedResult.isDone()).toBe(false);
+
+    cleanupDeferrable.resolve();
+
+    await flushAllMicrotasks();
+    expect(actionToRetryFn).toHaveBeenCalledTimes(2);
+    expect(retriedResult.isDone()).toBe(true);
+    await expect(retriedResult).rejects.toThrow('Oops!');
+  });
+});
+
+describe(withInspection, () => {
+  it('returns the original promise when resolved', async () => {
+    const {promise, resolve} = createDecomposedPromise<number>();
+    const inspectablePromise = withInspection(promise);
+    resolve(42);
+    await expect(inspectablePromise).resolves.toBe(42);
+  });
+
+  it('returns the original promise when rejected', async () => {
+    const {promise, reject} = createDecomposedPromise<number>();
+
+    const inspectablePromise = withInspection(promise);
+    const handledPromise = expect(inspectablePromise).rejects.toThrow('Oops!');
+    reject(new Error('Oops!'));
+    await handledPromise;
+  });
+
+  it('indicates if the promise is done when resolved', async () => {
+    const {promise, resolve} = createDecomposedPromise<number>();
+    const inspectablePromise = withInspection(promise);
+
+    expect(inspectablePromise.isDone()).toBe(false);
+
+    resolve(42);
+    await expect(inspectablePromise).resolves.toBe(42);
+
+    expect(inspectablePromise.isDone()).toBe(true);
+  });
+
+  it('indicates if the promise is done when rejected', async () => {
+    const {promise, reject} = createDecomposedPromise<number>();
+    const inspectablePromise = withInspection(promise);
+
+    expect(inspectablePromise.isDone()).toBe(false);
+
+    const handledPromise = expect(inspectablePromise).rejects.toThrow('Oops!');
+    reject(new Error('Oops!'));
+    await handledPromise;
+
+    expect(inspectablePromise.isDone()).toBe(true);
+  });
+
+  it('provides debug values when the promise is resolved', async () => {
+    const {promise, resolve} = createDecomposedPromise<number>();
+    const inspectablePromise = withInspection(promise);
+
+    resolve(42);
+    await expect(inspectablePromise).resolves.toBe(42);
+
+    const {resolvedValue, rejectionError} = inspectablePromise.getDebugValues();
+    expect(resolvedValue).toBe(42);
+    expect(rejectionError).toBeUndefined();
+  });
+
+  it('provides debug values when the promise is rejected', async () => {
+    const {promise, reject} = createDecomposedPromise<number>();
+    const inspectablePromise = withInspection(promise);
+
+    const error = new Error('Oops!');
+    const handledPromise = expect(inspectablePromise).rejects.toThrow('Oops!');
+    reject(error);
+    await handledPromise;
+
+    const {resolvedValue, rejectionError} = inspectablePromise.getDebugValues();
+    expect(resolvedValue).toBeUndefined();
+    expect(rejectionError).toBe(error);
+  });
+});
+
+describe(flushAllMicrotasks, () => {
+  it('flushes all pending microtasks', async () => {
+    const {promise, resolve} = createDecomposedPromise<number>();
+
+    const onFulfilled = jest.fn();
+    promise.then(onFulfilled);
+
+    resolve(42);
+    expect(onFulfilled).not.toHaveBeenCalled();
+
+    await flushAllMicrotasks();
+    expect(onFulfilled).toHaveBeenCalledTimes(1);
+    expect(onFulfilled).toHaveBeenCalledWith(42);
+  });
+
+  it('flushes microtasks enqueued by other microtasks', async () => {
+    const {promise: firstPromise, resolve: firstResolve} = createDecomposedPromise<number>();
+    const {promise: secondPromise, resolve: secondResolve} = createDecomposedPromise<number>();
+
+    firstPromise.then(() => {
+      secondResolve(42);
+    });
+
+    const onFulfilled = jest.fn();
+    secondPromise.then(onFulfilled);
+
+    firstResolve(21);
+    expect(onFulfilled).not.toHaveBeenCalled();
+
+    await flushAllMicrotasks();
+    expect(onFulfilled).toHaveBeenCalledTimes(1);
+    expect(onFulfilled).toHaveBeenCalledWith(42);
+  });
+});
---
+++ src/common/promises.ts
@@ -0,0 +1,170 @@
+export class TimeoutError extends Error {}
+export class AbortError extends Error {}
+export class TimeoutSourceLateRejectionError extends Error {
+  constructor(public originalRejection: unknown) {
+    super();
+  }
+}
+
+export interface InspectablePromise<T> extends Promise<T> {
+  isDone(): boolean;
+  getDebugValues(): {resolvedValue: unknown; rejectionError: Error | undefined};
+}
+
+export interface DecomposedPromise<T> {
+  /** A function to resolve the attached promise. */
+  resolve(value: T): void;
+  /** A function to reject the attached promise. */
+  reject(err: Error): void;
+  /** The promise controlled by the resolve and reject methods. */
+  promise: Promise<T>;
+}
+
+export interface PromiseTimeoutOptions<T> {
+  /** The elapsed time in milliseconds before the promise will reject. Values of infinity will return the original promise. */
+  timeoutMs: number;
+  /** The custom error message to use when the timeout is reached. */
+  timeoutErrorMessage?: string;
+  /** The custom error message to use when the promise is aborted. */
+  abortErrorMessage?: string;
+  /** The abort controller to both listen for changes to abort early, and to invoke abort when timeout is reached. */
+  abortController?: AbortController;
+  /** The function to invoke if the promise eventually resolves with a value. */
+  cleanupOnResolve?(value: T): void;
+  /** The function to invoke if the promise eventually rejects with a value. */
+  cleanupOnReject?(error: Error): void;
+}
+
+/** Creates the a promise with resolve and reject methods exposed. */
+export function createDecomposedPromise<T>(): DecomposedPromise<T> {
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  let resolve = (value: T) => {};
+  let reject = (err: Error) => {};
+  /* eslint-enable @typescript-eslint/no-unused-vars */
+
+  const promise = new Promise<T>((resolve_, reject_) => {
+    resolve = resolve_;
+    reject = reject_;
+  });
+
+  return {resolve, reject, promise};
+}
+
+/** Adds timeout management to an existing promise, including abortController notification, and */
+export function withTimeout<T>(
+  sourcePromise: Promise<T>,
+  options: PromiseTimeoutOptions<T>
+): Promise<T> {
+  if (!Number.isFinite(options.timeoutMs)) return sourcePromise;
+
+  const {resolve, reject, promise} = createDecomposedPromise<T>();
+
+  let hasCancelled = false;
+  const timeout = setTimeout(() => {
+    hasCancelled = true;
+    const errorMessage =
+      options.timeoutErrorMessage || `Operation exceeded ${options.timeoutMs}ms timeout`;
+    const error = new TimeoutError(errorMessage);
+
+    reject(error);
+    if (options?.abortController) options.abortController.abort(error);
+  }, options.timeoutMs);
+
+  if (options.abortController) {
+    options.abortController.signal.addEventListener('abort', () => {
+      if (hasCancelled) return;
+
+      hasCancelled = true;
+      const errorMessage = options.abortErrorMessage || `Operation aborted`;
+      reject(new AbortError(errorMessage));
+    });
+  }
+
+  sourcePromise
+    .then((value) => {
+      resolve(value);
+      clearTimeout(timeout);
+
+      if (hasCancelled && options.cleanupOnResolve) options.cleanupOnResolve(value);
+    })
+    .catch((rejection) => {
+      reject(rejection);
+      clearTimeout(timeout);
+      const error =
+        rejection instanceof Error ? rejection : new TimeoutSourceLateRejectionError(rejection);
+
+      if (hasCancelled && options.cleanupOnReject) options.cleanupOnReject(error);
+    });
+
+  return promise;
+}
+
+/** A function to retry an action on failure with optional cleanup. */
+export async function withRetry<T>(
+  actionToRetry: () => Promise<T>,
+  options: {
+    /** The number of times to retry the action before failing. */
+    retries: number;
+    /** The optional async cleanup to perform after each failure, the result of cleanup will be awaited before retrying. */
+    cleanup?: () => Promise<void>;
+  }
+): Promise<T> {
+  let lastError: unknown;
+  for (let i = 0; i <= options.retries; i++) {
+    try {
+      const result = await actionToRetry();
+      return result;
+    } catch (err) {
+      lastError = err;
+      if (options.cleanup) await options.cleanup();
+    }
+  }
+
+  throw lastError;
+}
+
+/**
+ * Flushes all pending microtasks (and microtasks that _they_ enqueue) from the queue.
+ * @see https://gist.github.com/patrickhulce/0c4c7b6b05937b6854b048a20b7ddb6c
+ * @returns void
+ */
+export async function flushAllMicrotasks() {
+  // 1000 is not significant, any number high enough to ensure all trampolined microtasks are
+  // executed will do.
+  for (let i = 0; i < 1000; i++) {
+    await Promise.resolve();
+  }
+}
+
+/**
+ * Augments a promise to enable synchronous inspection of whether it has settled.
+ *
+ * @param promise The promise to make inspectable.
+ * @returns The inspectable promise.
+ */
+export function withInspection<T>(promise: Promise<T>): InspectablePromise<T> {
+  let hasSettled = false;
+  let resolvedValue: T | undefined = undefined;
+  let rejectionError: Error | undefined = undefined;
+
+  const inspectablePromise = promise
+    .then((value) => {
+      hasSettled = true;
+      resolvedValue = value;
+      return value;
+    })
+    .catch((err) => {
+      hasSettled = true;
+      rejectionError = err;
+      throw err;
+    });
+
+  return Object.assign(inspectablePromise, {
+    isDone() {
+      return hasSettled;
+    },
+    getDebugValues() {
+      return {resolvedValue, rejectionError};
+    },
+  });
+}
